c5dfc2accecee8dd6715f3c072b38c5d08bd1210
Nathan Letwory <nathan@mcneel.com>
2017-11-23 09:05:07 +0200

    Initial Blend material setup.

---
+++ RaytracedBlendMaterial/Properties/AssemblyInfo.cs
@@ -0,0 +1,74 @@
+﻿/**
+Copyright 2017 Robert McNeel and Associates
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional.
+// These will show in Rhino's option dialog, in the tab Plug-ins.
+[assembly: PlugInDescription(DescriptionType.Address, "-")]
+[assembly: PlugInDescription(DescriptionType.Country, "-")]
+[assembly: PlugInDescription(DescriptionType.Email, "-")]
+[assembly: PlugInDescription(DescriptionType.Phone, "-")]
+[assembly: PlugInDescription(DescriptionType.Fax, "-")]
+[assembly: PlugInDescription(DescriptionType.Organization, "-")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "-")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "-")]
+
+// Icons should be Windows .ico files and contain 32-bit images in the following sizes: 16, 24, 32, 48, and 256.
+// This is a Rhino 6-only description.
+[assembly: PlugInDescription(DescriptionType.Icon, "RaytracedBlendMaterial.EmbeddedResources.plugin-utility.ico")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RaytracedBlendMaterial")]
+
+// This will be used also for the plug-in description.
+[assembly: AssemblyDescription("RaytracedBlendMaterial utility plug-in")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RaytracedBlendMaterial")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d4c6802-cc84-4e73-82e7-86ec6ecfbbc8")] // This will also be the Guid of the Rhino plug-in
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Make compatible with Rhino Installer Engine
+[assembly: AssemblyInformationalVersion("2")]
---
+++ RaytracedBlendMaterial/RaytracedBlendMaterial.cs
@@ -0,0 +1,64 @@
+﻿/**
+Copyright 2017 Robert McNeel and Associates
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
+
+using Rhino.Render;
+using RhinoCyclesCore.Materials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ccl;
+using RhinoCyclesCore;
+using RhinoCyclesCore.ExtensionMethods;
+using Rhino.Display;
+
+namespace RaytracedBlendMaterial
+{
+	public class RaytracedBlendMaterial : RenderMaterial, ICyclesMaterial
+	{
+		public override string TypeName => "Raytraced Blend Material";
+
+		public override string TypeDescription => TypeName;
+
+		public ShaderBody.CyclesMaterial MaterialType => ShaderBody.CyclesMaterial.CustomRenderMaterial;
+
+		public string MaterialXml => throw new InvalidOperationException("Raytraced Blend Material is not an XML-based material");
+
+		public float Gamma { get; set; }
+
+		public RaytracedBlendMaterial()
+		{
+			Fields.AddTextured("mat1", Color4f.White, "Material 1");
+			Fields.AddTextured("blend", 0.5f, "Blend factor");
+			Fields.AddTextured("mat2", null, "Material 2");
+		}
+
+		public void BakeParameters()
+		{
+		}
+
+		public bool GetShader(Shader sh)
+		{
+			ccl.ShaderNodes.DiffuseBsdfNode diffuseBsdf = new ccl.ShaderNodes.DiffuseBsdfNode();
+			diffuseBsdf.ins.Color.Value = Color4f.White.ToFloat4();
+			sh.AddNode(diffuseBsdf);
+			diffuseBsdf.outs.BSDF.Connect(sh.Output.ins.Surface);
+
+			return true;
+		}
+	}
+}
